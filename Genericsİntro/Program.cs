7fddbffe4a2b473f9950a6eccec86c9039c60164
ahmme <ahmme@DESKTOP-3FL38SJ>
2021-01-20 23:53:38 +0300

    Add project files.

---
+++ Genericsİntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Genericsİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isim = new MyList<string>();
+            isim.Add("Sinem");
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isim = new string[] { "Ahmet", "Merve", "Elif", "Ayşe", "Hatice" };
+            Console.WriteLine(isim[0]);
+            Console.WriteLine(isim[1]);
+            Console.WriteLine(isim[2]);
+            Console.WriteLine(isim[3]);
+            Console.WriteLine(isim[4]);
+
+            List<string> isim2 = new List<string>() { "Ahmet", "Merve", "Elif", "Ayşe", "Hatice" };
+            Console.WriteLine(isim2[0]);
+            Console.WriteLine(isim2[1]);
+            Console.WriteLine(isim2[2]);
+            Console.WriteLine(isim2[3]);
+            Console.WriteLine(isim2[4]);
+            isim2.Add("Linda");
+            Console.WriteLine(isim2[5]);
+        }
+    }
+}
